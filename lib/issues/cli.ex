60a72fc7db5fc7a4b74202c5e30a15bac78d5308
Hannes Cattrysse <f2ostie@gmail.com>
2016-07-05 16:10:35 +0200

    added http client and json parsing

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,16 +1,11 @@
 defmodule Issues.CLI do
 
-  @default_count 4
 
   @moduledoc """
   Handle the command line parsing and the dispatch to
   the various functions that end up generating a
   table of the last _n_ issues in a github project
   """
-
-  def run(argv) do
-    parse_args(argv)
-  end
 
   @doc """
   `argv` can be -h or --help, which returns :help.
@@ -20,13 +15,13 @@ defmodule Issues.CLI do
 
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
+  @default_count 4
 
-
-    def run(argv) do
-      argv
-      |> parse_args
-      |> process
-    end
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
 
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [ help: :boolean],
@@ -49,8 +44,22 @@ defmodule Issues.CLI do
 
   def process(:help) do
     IO.puts """
-    usage: issues <user> <project> [count |
+    usage: issues <user> <project> [count | #{@default_count} ]
+    """
+    System.halt(0)
   end
 
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+  end
+
+  def decode_response({:ok, body}), do body
+
+  def decode_response({:error, body}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
 
 end
